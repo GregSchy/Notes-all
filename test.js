1f16cfe20d4fc36a129bc7fae2ce7af8aad81be4
Grzegorz Szymalak <105386198+GregSchy@users.noreply.github.com>
2023-09-08 21:33:20 +0200

    s

--- test.js
+++ test.js
@@ -1,21 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
-
-export default function App() {
-  const [name, setName] = useState("");
-  const inputRef = useRef();
-
-  function focus() {
-    inputRef.current.focus();
-  }
-  return (
-    <>
-      <input
-        ref={inputRef}
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <div>My name is {name}</div>
-      <button onClick={focus}>focus</button>
-    </>
-  );
-}
+Write a function solution that, given an array A of N integers
+(between -100 and 100), returns the sign (-1, 0, 1) of the
+product of all the numbers in the array multiplied together.
+Assume that N is between 1 and 1000.
+For example, given A = [1, -2, -3, 5], the function should return
+1 (the multiplication equals 30).
+Given A = [1, 2, 3, -5] your function should return -1 (the
+multiplication equals -30)
+Given A = [1, 2, 0, -5] your function should return O (the
+multiplication equals 0).
+Copyright 2009-2023 by Codility Limited. All
